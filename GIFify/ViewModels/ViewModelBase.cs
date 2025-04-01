75ac0787939d515c10a34afd7b9ab6f2aa4c9774
Fess00 <ss_s0l0@mail.ru>
2025-04-01 23:01:12 +0300

    Добавьте файлы проекта.

---
+++ GIFify/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace GIFify.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ GIFify/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GIFify.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
